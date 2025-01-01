cad067e55be759c4e486c63e0cb84e0f5afc1e42
FlorianMichael <florian.michael07@gmail.com>
2025-01-01 01:07:59 +0100

    Auto: 2025

--- src/main/java/de/florianmichael/waybackauthlib/InvalidCredentialsException.java
+++ src/main/java/de/florianmichael/waybackauthlib/InvalidCredentialsException.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of WaybackAuthLib - https://github.com/FlorianMichael/WaybackAuthLib
- * Copyright (C) 2023-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
+ * Copyright (C) 2023-2025 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
